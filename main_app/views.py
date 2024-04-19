42bb82495c1b78f24ff901e55419fe2b383e1c25
Ryan Callebs <ryan.callebs@gmail.com>
2024-04-19 14:14:18 -0400

    add home page view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
