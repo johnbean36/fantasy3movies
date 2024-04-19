700a861b68a229602f45039aa8645870c9c96dd6
Ryan Callebs <ryan.callebs@gmail.com>
2024-04-19 16:22:42 -0400

    login and signup functions active

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('movies', views.movies_index, name='index' ),
-    path('movies/detail', views.movies_detail, )
+    path('movies/detail', views.movies_detail, ),
+    path('accounts/signup', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
+# Create your views here.
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +13,19 @@ def movies_index(request):
 
 def movies_detail(request):
     return render (request, 'movies/detial.html')
+
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
