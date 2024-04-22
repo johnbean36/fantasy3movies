e1c6a0a0ea641ddcd30def9cf9d8f704f0db4e5c
johnbean36 <jmbass36@gmail.com>
2024-04-22 09:43:31 -0500

    Shows production members when you make them

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('movies/actor/', views.AddActor.as_view(), name='actors'),
     path('movies/production/', views.AddProd_Mem.as_view(), name='production'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('movies/actor/<int:pk>/delete/', views.DeleteActor.as_view(), name='delete_actor')
+    path('movies/actor/<int:pk>/delete/', views.DeleteActor.as_view(), name='delete_actor'),
+    path('movies/prod_mem/<int:pk>/delete/', views.DeleteProd.as_view(), name='delete_prod')
 ]
